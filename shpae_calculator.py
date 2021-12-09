2af819d985b3d3702959304c9bdeb317d32ee259
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-09 17:37:23 +0800

    Set init method for the rectangle class

--- shpae_calculator.py
+++ shpae_calculator.py
@@ -3,8 +3,12 @@ class Rectangle:
     Consist following methods: set_width, set_height, get_area,
     get_perimeter, get_diagonal, get_picture, get_amount_inside
     """
-
-
+    def __init__(self, width, height):
+        """
+        Initialize with width and height attributes
+        """
+        self.width = width
+        self.height = height
 
 
 # class Square:

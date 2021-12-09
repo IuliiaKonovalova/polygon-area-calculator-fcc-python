ad1562d10b2ee3e7d2c63e2de00e81093ae7b476
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-12-09 17:35:17 +0800

    Add rectangle class

---
+++ shpae_calculator.py
@@ -0,0 +1,10 @@
+class Rectangle:
+    """
+    Consist following methods: set_width, set_height, get_area,
+    get_perimeter, get_diagonal, get_picture, get_amount_inside
+    """
+
+
+
+
+# class Square:
